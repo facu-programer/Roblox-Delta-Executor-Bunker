f29a951da946eeb7e0cc5e1ceae114948c4ab0b4
Facundo Efimenco <facu22251@gmail.com>
2025-11-06 09:05:10 -0300

    commit

--- subir.bat
+++ subir.bat
@@ -3,5 +3,5 @@ git add .
 git commit -m "commit"
 git remote add origin https://github.com/facu-programer/Roblox-Delta-Executor-Bunker.git
 git branch -M main
-git push -u origin main --force
+git push -u origin main --rebase
 pause
