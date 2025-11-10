4b068c32146beaab208993cf3431d422d45e9df5
Facundo Efimenco <facu22251@gmail.com>
2025-11-09 23:27:27 -0300

    commit

--- GuiLogic.lua
+++ GuiLogic.lua
@@ -12,13 +12,24 @@ end)
 
 local gui = game.Players.LocalPlayer.PlayerGui
 
+local stop = Instance.new("ImageButton")
+stop.Image = "rbxassetid://98939136188247"
+stop.BackgroundColor3 = gui.PScreen.ScrollingFrame.BackgroundColor3
+stop.BorderSizePixel = 0
+stop.BorderColor3 = Color3.fromRGB(255, 255, 255)
+stop.Parent = gui.PScreen.ScrollingFrame
+
+stop.MouseButton1Click:Connect(function()
+	game.Players.LocalPlayer.PlayerGui.PScreen.Enabled = false
+end)
+
 local autoCollectFood = Instance.new("TextButton")
 
 autoCollectFood.Text = "Auto Collect Food"
 autoCollectFood.BackgroundColor3 = gui.PScreen.ScrollingFrame.BackgroundColor3
 autoCollectFood.TextScaled = true
 autoCollectFood.TextColor3 = Color3.new(1, 1, 1)
-autoCollectFood.BorderSizePixel = 8
+autoCollectFood.BorderSizePixel = 4
 autoCollectFood.BorderColor3 = Color3.fromRGB(255, 255, 255)
 autoCollectFood.Parent = gui.PScreen.ScrollingFrame
 
